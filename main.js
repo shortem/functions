f7e9c45c77dd97324e1e4eb23b4facea84d3899e
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2021-09-28 10:26:57 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 // Math 
-const math = r('https://raw.githubusercontent.com/shortem/functions/main/scripts/math.js');
-math.toEight = math.toEight;
-math.toTwo = math.toTwo;
+const mathFunction = r('https://raw.githubusercontent.com/shortem/functions/main/scripts/math.js');
+math.toEight = mathFunction.toEight;
+math.toTwo = mathFunction.toTwo;
 
