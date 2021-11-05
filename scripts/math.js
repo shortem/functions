4633da1bdc8ff691a4c3111e832e84034e337ec3
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2021-11-05 12:33:47 +0800

    Update math.js

--- scripts/math.js
+++ scripts/math.js
@@ -6,5 +6,10 @@ function toTwo(number){
  return (number).toString(2);
 }
 
+function days(before, after) {
+var dayBegin = new Date(before);
+var dayAfter = new Date(after);
+return Math.round((dayAfter - dayBegin) / (1000 * 60 * 60 * 24))
+}
 
-module.exports = { toEight, toTwo }
+module.exports = { toEight, toTwo, days }
