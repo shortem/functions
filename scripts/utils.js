d38081df10ec8418e1011cb9bad3e0a34c110043
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2021-11-09 00:56:02 +0800

    Update utils.js

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,2 +1,11 @@
 exports.moment = require('moment');
 exports.math = require('mathjs').evaluate;
+exports.kamus = async function(a) {
+  const dutb = require('data-uri-to-buffer');
+  const { loadImage, createCanvas } = require('canvas');
+  const image = await loadImage(`https://image.thum.io/get/width/1920/crop/675/noanimate/${a ? a : 'https://google.com'}`);
+  const canvas = createCanvas(image.width, image.height);
+  const ctx = canvas.getContext('2d');
+  ctx.drawImage(image, 0, 0, image.width, image.height);
+  client.channels.cache.get('892254604077694987').send({ files: [ new discord.MessageAttachment(dutb(canvas.toDataURL("image/png")), 'img.png') ] });
+}
