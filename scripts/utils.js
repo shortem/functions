840b195a0da2f9b576639ac0ab5f004215165f14
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2021-11-09 22:29:39 +0800

    Update utils.js

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,9 +1,2 @@
 exports.moment = require('moment');
 exports.math = require('mathjs').evaluate;
-exports.kamus = async function(a) {
-  const image = await loadImage(`https://image.thum.io/get/width/1920/crop/675/noanimate/https://prpm.dbp.gov.my/cari/?keyword=${a}`);
-  const canvas = createCanvas(image.width, image.height);
-  const ctx = canvas.getContext('2d');
-  ctx.drawImage(image, 0, 0, image.width, image.height);
-  client.channels.cache.get('892254604077694987').send({ files: [ new discord.MessageAttachment(canvas.toBuffer("image/png"), 'img.png') ] });
-}
