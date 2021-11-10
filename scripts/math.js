c24716f631907fbf81f4d02e8a5816b5b9ad3f46
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2021-11-10 22:49:57 +0800

    Update math.js

--- scripts/math.js
+++ scripts/math.js
@@ -12,4 +12,10 @@ var dayAfter = new Date(after);
 return Math.round((dayAfter - dayBegin) / (1000 * 60 * 60 * 24))
 }
 
-module.exports = { toEight, toTwo, days }
+function neutral(boolean, number) {
+  if(boolean == true && number == 0) return console.log('This is not a 自然数 (neutral number)');
+  if(boolean == false)return console.log('This is a 自然数 （neutral number)');
+  return console.log('Please provide a number and an option');
+}
+
+module.exports = { toEight, toTwo, days, neutral }
